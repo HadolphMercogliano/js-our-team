78a838075f3028004b88f53cf5634eca80b741aa
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-02-06 17:11:38 +0100

    milestone 0 end

---
+++ js/main.js
@@ -0,0 +1,53 @@
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+
+// MILESTONE 1:
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+// BONUS 1:
+// Trasformare la stringa foto in una immagine effettiva
+
+// BONUS 2:
+// Organizzare i singoli membri in card/schede
+
+const Team = [
+  {
+    nome: "Wayne Barnett",
+    ruolo: "Founder & CEO",
+    foto: "../img/wayne-barnett-founder-ceo.jpg",
+  },
+  {
+    nome: "Angela Caroll",
+    ruolo: "Chief Editor",
+    foto: "../img/angela-caroll-chief-editor.jpg",
+  },
+  {
+    nome: "Walter Gordon",
+    ruolo: "Office Manager",
+    foto: "../img/walter-gordon-office-manager.jpg",
+  },
+  {
+    nome: "Angela Lopez",
+    ruolo: "Angela Lopez",
+    foto: "../img/angela-lopez-social-media-manager.jpg",
+  },
+  {
+    nome: "Scott Estrada",
+    ruolo: "Developer",
+    foto: "../img/scott-estrada-developer.jpg",
+  },
+  {
+    nome: "Barbara Ramos",
+    ruolo: "Graphic Designer",
+    foto: "../img/barbara-ramos-graphic-designer.jpg",
+  },
+];
+for (const component of Team) {
+  console.log(component);
+}

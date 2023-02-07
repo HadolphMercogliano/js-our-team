138ac8049bfcc16fad761d15892bb6be18ae8df0
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-02-07 10:06:11 +0100

    foto string correction

--- js/main.js
+++ js/main.js
@@ -20,40 +20,35 @@ const Team = [
   {
     nome: "Wayne Barnett",
     ruolo: "Founder & CEO",
-    foto: "../img/wayne-barnett-founder-ceo.jpg",
+    foto: "./img/wayne-barnett-founder-ceo.jpg",
   },
   {
     nome: "Angela Caroll",
     ruolo: "Chief Editor",
-    foto: "../img/angela-caroll-chief-editor.jpg",
+    foto: "./img/angela-caroll-chief-editor.jpg",
   },
   {
     nome: "Walter Gordon",
     ruolo: "Office Manager",
-    foto: "../img/walter-gordon-office-manager.jpg",
+    foto: "./img/walter-gordon-office-manager.jpg",
   },
   {
     nome: "Angela Lopez",
     ruolo: "Social Media Manager",
-    foto: "../img/angela-lopez-social-media-manager.jpg",
+    foto: "./img/angela-lopez-social-media-manager.jpg",
   },
   {
     nome: "Scott Estrada",
     ruolo: "Developer",
-    foto: "../img/scott-estrada-developer.jpg",
+    foto: "./img/scott-estrada-developer.jpg",
   },
   {
     nome: "Barbara Ramos",
     ruolo: "Graphic Designer",
-    foto: "../img/barbara-ramos-graphic-designer.jpg",
+    foto: "./img/barbara-ramos-graphic-designer.jpg",
   },
 ];
 
-// const team2 = {
-//   nome: "Barbara Ramos",
-//   ruolo: "Graphic Designer",
-//   foto: "../img/barbara-ramos-graphic-designer.jpg",
-// };
 for (const property of Team) {
   console.log(property.nome);
   console.log(property.ruolo);
